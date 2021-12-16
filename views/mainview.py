c295718aef753d419470377e0860b869c347fcb8
Offlinexaa <Offlinexaa@gmail.com>
2021-12-16 22:02:22 +0400

    Добавил штатный выход.

--- views/mainview.py
+++ views/mainview.py
@@ -13,3 +13,16 @@ def main_page():
     flask.session['uid'] = uuid.uuid4()
     check_and_create_dirs()
     return flask.render_template('main_page.html')
+
+
+def shutdown_server():
+    func = flask.request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@blueprint.route('/shutdown/')
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
